499f5937417c35f9b39dc7e13d857a75b96bf055
Stéphane Caron <stephane.caron@normalesup.org>
2022-05-04 12:31:43 +0200

    Bullet utility function to find the index of a link

--- actuation/bullet_utils.h
+++ actuation/bullet_utils.h
@@ -28,6 +28,7 @@ namespace vulp::actuation {
 /*! Convert an Eigen vector to a Bullet one.
  *
  * \param[in] v Eigen vector.
+ *
  * \return Same vector for Bullet.
  */
 inline btVector3 bullet_from_eigen(const Eigen::Vector3d& v) {
@@ -37,6 +38,7 @@ inline btVector3 bullet_from_eigen(const Eigen::Vector3d& v) {
 /*! Convert a Bullet quaternion to an Eigen one.
  *
  * \param[in] quat Bullet quaternion.
+ *
  * \return Same vector for Eigen.
  */
 inline Eigen::Quaterniond eigen_from_bullet(const btQuaternion& quat) {
@@ -46,10 +48,32 @@ inline Eigen::Quaterniond eigen_from_bullet(const btQuaternion& quat) {
 /*! Convert a Bullet vector to an Eigen one.
  *
  * \param[in] quat Bullet vector.
+ *
  * \return Same vector for Eigen.
  */
 inline Eigen::Vector3d eigen_from_bullet(const btVector3& v) {
   return Eigen::Vector3d(v.getX(), v.getY(), v.getZ());
+}
+
+/*! Find the index of a link.
+ *
+ * \param[in] bullet Bullet client.
+ * \param[in] robot Index of the robot to search.
+ * \param[in] link_name Name of the searched link.
+ *
+ * \return Link index if found, -1 otherwise.
+ */
+inline int find_link_index(b3RobotSimulatorClientAPI& bullet, int robot,
+                           const std::string& link_name) noexcept {
+  b3JointInfo joint_info;
+  int nb_joints = bullet.getNumJoints(robot);
+  for (int joint_index = 0; joint_index < nb_joints; ++joint_index) {
+    bullet.getJointInfo(robot, joint_index, &joint_info);
+    if (std::string(joint_info.m_linkName) == link_name) {
+      return joint_index;  // link and joint indices are the same in Bullet
+    }
+  }
+  return -1;
 }
 
 /*! Compute IMU readings from the IMU link state.
