ffd2074d6ffe7180901a7fc554d11cf135413f68
Stéphane Caron <stephane.caron@normalesup.org>
2022-05-12 19:26:06 +0200

    Add //tools/shm:clean to clean up shared memory

---
+++ tools/shm/clean.cpp
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2022 Stéphane Caron
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <fcntl.h>
+#include <spdlog/spdlog.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace tools::shm {
+
+//! Command-line arguments.
+class CommandLineArguments {
+ public:
+  /*! Read command line arguments.
+   *
+   * \param[in] args List of command-line arguments.
+   */
+  explicit CommandLineArguments(const std::vector<std::string>& args) {
+    for (size_t i = 0; i < args.size(); i++) {
+      const auto& arg = args[i];
+      if (arg == "-h" || arg == "--help") {
+        help = true;
+      } else if (arg[0] != '-') {
+        filename = args.at(i);
+      } else {
+        spdlog::error("Unknown argument: {}", arg);
+        error = true;
+      }
+    }
+  }
+
+  /*! Show help message
+   *
+   * \param[in] name Binary name from argv[0].
+   */
+  inline void print_usage(const char* name) noexcept {
+    std::cout << "Usage: " << name << " <filename> [options]\n";
+    std::cout << "\n";
+    std::cout << "Optional arguments:\n\n";
+    std::cout << "-h, --help\n"
+              << "    Print this help and exit.\n";
+    std::cout << "-s, --show\n"
+              << "    Show the Bullet GUI.\n";
+    std::cout << "\n";
+  }
+
+ public:
+  //! Error flag
+  bool error = false;
+
+  //! Help flag
+  bool help = false;
+
+  //! Name of shared memory file
+  std::string filename = "/vulp";
+};
+
+int main(const CommandLineArguments& args) {
+  const char* name = args.filename.c_str();
+  int file_descriptor = ::shm_open(name, O_RDWR, 0666);
+  if (file_descriptor < 0) {
+    if (errno == ENOENT) {
+      spdlog::warn("File {} did not exist, nothing to clean", name);
+    } else if (errno == EINVAL) {
+      spdlog::error("errno == EINVAL: The name argument was invalid.");
+    } else {
+      spdlog::error("Error opening file; errno is {}", errno);
+    }
+    return EXIT_FAILURE;
+  }
+  if (::shm_unlink(name) < 0) {
+    spdlog::error("Failed to unlink shared memory; errno is {}", errno);
+  }
+  return EXIT_SUCCESS;
+}
+
+}  // namespace tools::shm
+
+int main(int argc, char** argv) {
+  tools::shm::CommandLineArguments args({argv + 1, argv + argc});
+  if (args.error) {
+    return EXIT_FAILURE;
+  } else if (args.help) {
+    args.print_usage(argv[0]);
+    return EXIT_SUCCESS;
+  }
+  return tools::shm::main(args);
+}
