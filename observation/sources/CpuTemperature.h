a9981e69af15c962bc64060514873940b7c6a208
Stéphane Caron <stephane.caron@normalesup.org>
2023-03-01 18:42:30 +0100

    Add disabled state to CPU temperature observer

--- observation/sources/CpuTemperature.h
+++ observation/sources/CpuTemperature.h
@@ -46,6 +46,9 @@ class CpuTemperature : public Source {
    */
   void write(Dictionary& observation) final;
 
+  //! Check if temperature observations are disabled.
+  bool is_disabled() const { return is_disabled_; }
+
  private:
   /*! Issue a warning if the temperature is too high.
    *
@@ -56,13 +59,16 @@ class CpuTemperature : public Source {
   void check_temperature_warning(const double temperature) noexcept;
 
  private:
-  //! File descriptor to the kernel virtual file
+  //! Write function does nothing if this flag is set.
+  bool is_disabled_ = false;
+
+  //! File descriptor to the kernel virtual file.
   int fd_;
 
   //! Buffer to hold the temperature string read from file.
   char buffer_[kCpuTemperatureBufferSize];
 
-  //! Flag set to true when issuing a temperature warning
+  //! Flag set to true when issuing a temperature warning.
   bool has_warned_;
 };
 
