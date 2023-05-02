7cffcdd00a67cf0df76f56ce571691c419b71b3b
boragokbakan <bgokbakan@gmail.com>
2023-05-02 14:34:41 +0200

    Mock realtime for macOS

--- utils/realtime.h
+++ utils/realtime.h
@@ -34,7 +34,7 @@ namespace vulp::utils {
  * \throw std::runtime_error If the operation failed.
  */
 inline void configure_cpu(int cpu) {
-  #ifndef __APPLE__
+#ifndef __APPLE__
   constexpr int kPid = 0;  // this thread
   cpu_set_t cpuset = {};
   CPU_ZERO(&cpuset);
@@ -42,9 +42,10 @@ inline void configure_cpu(int cpu) {
   if (::sched_setaffinity(kPid, sizeof(cpu_set_t), &cpuset) < 0) {
     throw std::runtime_error("Error setting CPU affinity");
   }
-  #else
-  spdlog::warn("This function has not been implemented for MacOS, doing nothing...");
-  #endif
+#else
+  spdlog::warn(
+      "This function has not been implemented for MacOS, doing nothing...");
+#endif
 }
 
 /*! Configure the scheduler policy to round-robin for this thread.
@@ -56,7 +57,7 @@ inline void configure_cpu(int cpu) {
  * \throw std::runtime_error If the operation failed.
  */
 inline void configure_scheduler(int priority) {
-  #ifndef __APPLE__
+#ifndef __APPLE__
   constexpr int kPid = 0;  // this thread
   struct sched_param params = {};
   params.sched_priority = priority;
@@ -64,12 +65,11 @@ inline void configure_scheduler(int priority) {
     throw std::runtime_error(
         "Error setting realtime scheduler, try running as root (use sudo)");
   }
-  #else
-  spdlog::warn("This function has not been implemented for MacOS, doing nothing...");
-  #endif
-
+#else
+  spdlog::warn(
+      "This function has not been implemented for MacOS, doing nothing...");
+#endif
 }
-
 
 /*! Lock all memory to RAM so that the kernel doesn't page it to swap.
  *
