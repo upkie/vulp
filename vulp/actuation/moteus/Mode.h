c3d816d06f90b7830d8c10df7e98d2bae8644c0c
Stéphane Caron <stephane.caron@normalesup.org>
2023-05-25 15:04:49 +0200

    Convert moteus mode to string

--- vulp/actuation/moteus/Mode.h
+++ vulp/actuation/moteus/Mode.h
@@ -88,4 +88,50 @@ enum class Mode {
   kPositionContinue = 42,  // Vulp-specific, not from moteus lib
 };
 
+/*! Name of a mode.
+ *
+ * \param[in] mode Mode to name.
+ */
+constexpr const char* to_string(const Mode& mode) noexcept {
+  switch (mode) {
+    case Mode::kStopped:
+      return "Mode::kStopped";
+    case Mode::kFault:
+      return "Mode::kFault";
+    case Mode::kEnabling:
+      return "Mode::kEnabling";
+    case Mode::kCalibrating:
+      return "Mode::kCalibrating";
+    case Mode::kCalibrationComplete:
+      return "Mode::kCalibrationComplete";
+    case Mode::kPwm:
+      return "Mode::kPwm";
+    case Mode::kVoltage:
+      return "Mode::kVoltage";
+    case Mode::kVoltageFoc:
+      return "Mode::kVoltageFoc";
+    case Mode::kVoltageDq:
+      return "Mode::kVoltageDq";
+    case Mode::kCurrent:
+      return "Mode::kCurrent";
+    case Mode::kPosition:
+      return "Mode::kPosition";
+    case Mode::kPositionTimeout:
+      return "Mode::kPositionTimeout";
+    case Mode::kZeroVelocity:
+      return "Mode::kZeroVelocity";
+    case Mode::kStayWithinBounds:
+      return "Mode::kStayWithinBounds";
+    case Mode::kMeasureInductance:
+      return "Mode::kMeasureInductance";
+    case Mode::kBrake:
+      return "Mode::kBrake";
+    case Mode::kPositionContinue:
+      return "Mode::kPositionContinue";
+    default:
+      break;
+  }
+  return "?";
+}
+
 }  // namespace vulp::actuation::moteus
