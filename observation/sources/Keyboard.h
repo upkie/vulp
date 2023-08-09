31eb24bd72c86fdc890eb490185a5b341cf092c4
boragokbakan <bgokbakan@gmail.com>
2023-08-09 16:04:03 +0200

    reformatted files

--- observation/sources/Keyboard.h
+++ observation/sources/Keyboard.h
@@ -18,40 +18,49 @@
 
 #include <fcntl.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/select.h>
-#include <sys/ioctl.h>
-#include <string>
-#include <termios.h>
 #include <string.h>
-#include <iostream>
+#include <sys/ioctl.h>
+#include <sys/select.h>
+#include <termios.h>
+#include <unistd.h>
+
 #include <chrono>
+#include <iostream>
+#include <string>
 
 #include "vulp/observation/Source.h"
 
-#define MAX_KEY_BYTES 3 // Maximum number of bytes to encode a key
-#define KEY_POLLING_INTERVAL_MS 100 // Polling interval in milliseconds
+#define MAX_KEY_BYTES 3              // Maximum number of bytes to encode a key
+#define KEY_POLLING_INTERVAL_MS 100  // Polling interval in milliseconds
 
 // Byte sequences that encode arrow keys are platform specific
 #ifndef __APPLE__
 #define _HEAD 0xEF, 0x9C
-#define UP_BYTES {_HEAD, 0x80}
-#define DOWN_BYTES {_HEAD, 0x81}
-#define RIGHT_BYTES {_HEAD, 0x83}
-#define LEFT_BYTES {_HEAD, 0x82}
+#define UP_BYTES \
+  { _HEAD, 0x80 }
+#define DOWN_BYTES \
+  { _HEAD, 0x81 }
+#define RIGHT_BYTES \
+  { _HEAD, 0x83 }
+#define LEFT_BYTES \
+  { _HEAD, 0x82 }
 #else
 #define _HEAD 0x1B, 0x5B
-#define UP_BYTES {_HEAD, 0x41}
-#define DOWN_BYTES {_HEAD, 0x42}
-#define RIGHT_BYTES {_HEAD, 0x43}
-#define LEFT_BYTES {_HEAD, 0x44}
+#define UP_BYTES \
+  { _HEAD, 0x41 }
+#define DOWN_BYTES \
+  { _HEAD, 0x42 }
+#define RIGHT_BYTES \
+  { _HEAD, 0x43 }
+#define LEFT_BYTES \
+  { _HEAD, 0x44 }
 #endif
 
-#define is_lowercase_alpha(c) 0x61 <= c && c <= 0x7A // Checks if 'a' <= c <= 'z'
-#define is_uppercase_alpha(c) 0x41 <= c && c <= 0x5A // Checks if 'A' <= c <= 'Z'
-#define is_printable_ascii(c) 0x20 <= c && c <= 0x7F
-
-using namespace std::chrono;
+#define is_lowercase_alpha(c) \
+  0x61 <= c&& c <= 0x7A  // Checks if 'a' <= c <= 'z'
+#define is_uppercase_alpha(c) \
+  0x41 <= c&& c <= 0x5A  // Checks if 'A' <= c <= 'Z'
+#define is_printable_ascii(c) 0x20 <= c&& c <= 0x7F
 
 namespace vulp::observation::sources {
 
@@ -83,7 +92,7 @@ class Keyboard : public Source {
     D = 68,
     X = 88,
     // Unknown key
-    UNKNOWN = 255 // Map everything else to this key
+    UNKNOWN = 255  // Map everything else to this key
   };
 
   //! Prefix of output in the observation dictionary.
@@ -98,17 +107,17 @@ class Keyboard : public Source {
  private:
   //! Read next joystick event from the device file.
   int read_event();
-  
-  key map_char_to_key(unsigned char * buf);
 
-  unsigned char buf_[MAX_KEY_BYTES]; // Read ASCII and non-ASCII characters (e.g. arrows)
+  key map_char_to_key(unsigned char* buf);
 
-  key key_code_; // Key code of the last key pressed
-  bool key_pressed_; // Whether the last key pressed is still pressed
+  unsigned char
+      buf_[MAX_KEY_BYTES];  // Read ASCII and non-ASCII characters (e.g. arrows)
+
+  key key_code_;            // Key code of the last key pressed
+  bool key_pressed_;        // Whether the last key pressed is still pressed
 
   // Last time a key was pressed in milliseconds
   ::system_clock::time_point last_key_poll_time_;
-
 };
 
 }  // namespace vulp::observation::sources
