b36f99adda59aca8337465e749ee574bdb098b1e
Bora Gökbakan <82390536+boragokbakan@users.noreply.github.com>
2023-08-09 16:47:04 +0200

    Update observation/sources/Keyboard.cpp
    Fixed the copy-pasted copyright notice
    
    Co-authored-by: Stéphane Caron <stephane@scaron.info>

--- observation/sources/Keyboard.cpp
+++ observation/sources/Keyboard.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Stéphane Caron
+ * Copyright 2023 Inria
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
