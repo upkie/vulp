8ffdaf12e734436ec5f8b1d043496a657851ec2e
boragokbakan <bgokbakan@gmail.com>
2023-10-17 14:00:48 +0200

    Reenable voltage (16 bits) and temperature (8 bits)

--- vulp/actuation/resolution.h
+++ vulp/actuation/resolution.h
@@ -38,8 +38,8 @@ inline actuation::moteus::QueryCommand get_query_resolution() {
   query.q_current = Resolution::kIgnore;
   query.d_current = Resolution::kIgnore;
   query.rezero_state = Resolution::kIgnore;
-  query.voltage = Resolution::kIgnore;
-  query.temperature = Resolution::kIgnore;
+  query.voltage = Resolution::kInt16;
+  query.temperature = Resolution::kInt8;
   query.fault = Resolution::kInt8;
   return query;
 }
