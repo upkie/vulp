cd9980a50cc1eb6f3f453976ac39f0ccbdf13a2c
Stéphane Caron <stephane.caron@normalesup.org>
2023-12-22 11:30:47 +0100

    Keep existing moving-joint unit test

--- vulp/actuation/tests/BulletInterfaceTest.cpp
+++ vulp/actuation/tests/BulletInterfaceTest.cpp
@@ -159,6 +159,39 @@ TEST_F(BulletInterfaceTest, ComputeJointTorquesStopped) {
   ASSERT_NEAR(tau, 0.0, 1e-3);
 }
 
+TEST_F(BulletInterfaceTest, ComputeJointTorquesWhileMoving) {
+  const double no_position = std::numeric_limits<double>::quiet_NaN();
+  for (auto& command : data_.commands) {
+    command.mode = moteus::Mode::kPosition;
+    command.position.position = no_position;
+    command.position.velocity = 1.0;  // rev/s
+    command.position.kp_scale = 1.0;
+    command.position.kd_scale = 1.0;
+    command.position.maximum_torque = 1.0;  // N.m
+  }
+
+  // Cycle a couple of times so that both wheels spin up
+  interface_->cycle(data_, [](const moteus::Output& output) {});
+  interface_->cycle(data_, [](const moteus::Output& output) {});
+  interface_->cycle(data_, [](const moteus::Output& output) {});
+
+  // Right wheel has no kinetic friction
+  const auto& right_wheel = interface_->servo_reply().at("right_wheel").result;
+  const double right_target_velocity = right_wheel.velocity * (2.0 * M_PI);
+  const double right_torque = interface_->compute_joint_torque(
+      "right_wheel", no_position, right_target_velocity, 1.0, 1.0, 1.0);
+  ASSERT_GT(right_wheel.velocity, 0.1);
+  ASSERT_NEAR(right_torque, 0.0, 1e-3);
+
+  // Left wheel has kinetic friction
+  const auto& left_wheel = interface_->servo_reply().at("left_wheel").result;
+  const double left_target_velocity = left_wheel.velocity * (2.0 * M_PI);
+  const double left_torque = interface_->compute_joint_torque(
+      "left_wheel", no_position, left_target_velocity, 1.0, 1.0, 1.0);
+  ASSERT_GT(left_wheel.velocity, 0.1);                  // positive velocity
+  ASSERT_NEAR(left_torque, -kLeftWheelFriction, 1e-3);  // negative torque
+}
+
 TEST_F(BulletInterfaceTest, ComputeJointFeedforwardTorque) {
   const double no_position = std::numeric_limits<double>::quiet_NaN();
   for (auto& command : data_.commands) {
