b51b336c9b7f0b70a486a459bebcf3b0511e0109
Stéphane Caron <stephane.caron@normalesup.org>
2024-01-29 17:18:29 +0100

    Inline observe_time function

--- vulp/observation/observe_time.h
+++ vulp/observation/observe_time.h
@@ -13,6 +13,11 @@ using palimpsest::Dictionary;
  *
  * \param[out] observation Dictionary to write observations to.
  */
-void observe_time(Dictionary& observation);
+inline void observe_time(Dictionary& observation) {
+  const auto now = std::chrono::system_clock::now();
+  const auto time_since_epoch = now.time_since_epoch();
+  const auto nb_us = duration_cast<microseconds>(time_since_epoch).count();
+  observation("time") = static_cast<double>(nb_us) / 1e6;
+}
 
 }  // namespace vulp::observation
