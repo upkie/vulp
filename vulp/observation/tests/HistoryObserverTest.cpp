ad6531371565003955534b9c10c0b21163a95f31
Stéphane Caron <stephane.caron@inria.fr>
2024-05-24 18:03:03 +0200

    First unit test for history observer

---
+++ vulp/observation/tests/HistoryObserverTest.cpp
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2024 Inria
+
+#include <palimpsest/Dictionary.h>
+#include <palimpsest/exceptions/KeyError.h>
+
+#include <memory>
+
+#include "gtest/gtest.h"
+#include "vulp/observation/HistoryObserver.h"
+
+namespace vulp::observation::tests {
+
+using palimpsest::Dictionary;
+
+TEST(HistoryObserver, ObserveFloatingPointValues) {
+  // Writing after an empty read is the current spec, though not ideal
+  Dictionary observation;
+  HistoryObserver<double> history_observer(
+      /* keys = */ std::vector<std::string>{"servo", "left_knee", "torque"},
+      /* size = */ 3,
+      /* default_value = */ -1.0);
+  history_observer.write(observation);
+
+  const auto& values = observation("history")("servo")("left_knee")("torque")
+                           .as<std::vector<double>>();
+  ASSERT_EQ(values.size(), 3);
+  ASSERT_DOUBLE_EQ(values[0], -1.0);
+  ASSERT_DOUBLE_EQ(values[1], -1.0);
+  ASSERT_DOUBLE_EQ(values[2], -1.0);
+}
+
+}  // namespace vulp::observation::tests
