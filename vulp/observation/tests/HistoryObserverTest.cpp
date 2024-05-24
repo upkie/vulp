5ea228dac29c8784f491c07924e42edcc94a3916
Stéphane Caron <stephane.caron@inria.fr>
2024-05-24 18:05:27 +0200

    Add unit test for reading and writing doubles

--- vulp/observation/tests/HistoryObserverTest.cpp
+++ vulp/observation/tests/HistoryObserverTest.cpp
@@ -13,21 +13,44 @@ namespace vulp::observation::tests {
 
 using palimpsest::Dictionary;
 
-TEST(HistoryObserver, ObserveFloatingPointValues) {
-  // Writing after an empty read is the current spec, though not ideal
-  Dictionary observation;
+TEST(HistoryObserver, DoubleInitialization) {
   HistoryObserver<double> history_observer(
       /* keys = */ std::vector<std::string>{"servo", "left_knee", "torque"},
       /* size = */ 3,
       /* default_value = */ -1.0);
-  history_observer.write(observation);
 
+  Dictionary observation;
+
+  history_observer.write(observation);
   const auto& values = observation("history")("servo")("left_knee")("torque")
                            .as<std::vector<double>>();
   ASSERT_EQ(values.size(), 3);
   ASSERT_DOUBLE_EQ(values[0], -1.0);
   ASSERT_DOUBLE_EQ(values[1], -1.0);
   ASSERT_DOUBLE_EQ(values[2], -1.0);
+}
+
+TEST(HistoryObserver, DoubleReadThenWrite) {
+  HistoryObserver<double> history_observer(
+      /* keys = */ std::vector<std::string>{"servo", "left_knee", "torque"},
+      /* size = */ 3,
+      /* default_value = */ std::numeric_limits<double>::quiet_NaN());
+
+  Dictionary observation;
+  observation("servo")("left_knee")("torque") = 3.0;
+  history_observer.read(observation);
+  observation("servo")("left_knee")("torque") = 2.0;
+  history_observer.read(observation);
+  observation("servo")("left_knee")("torque") = 1.0;
+  history_observer.read(observation);
+
+  history_observer.write(observation);
+  const auto& values = observation("history")("servo")("left_knee")("torque")
+                           .as<std::vector<double>>();
+  ASSERT_EQ(values.size(), 3);
+  ASSERT_DOUBLE_EQ(values[0], 1.0);  // most recent comes first
+  ASSERT_DOUBLE_EQ(values[1], 2.0);
+  ASSERT_DOUBLE_EQ(values[2], 3.0);
 }
 
 }  // namespace vulp::observation::tests
