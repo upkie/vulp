f6405fd0b721b21c942c3fd9d29ec721e131c5ec
Stéphane Caron <stephane.caron@inria.fr>
2024-06-04 14:42:13 +0200

    Fix free-fall Bullet unit test

--- vulp/actuation/tests/BulletInterfaceTest.cpp
+++ vulp/actuation/tests/BulletInterfaceTest.cpp
@@ -341,12 +341,11 @@ TEST_F(BulletInterfaceTest, MonitorBaseState) {
 }
 
 TEST_F(BulletInterfaceTest, BasePositionFreeFall) {
-  Dictionary config, observation;
+  Dictionary config;
   Eigen::Vector3d base_position;
 
   interface_->reset(config);
-  interface_->observe(observation);
-  base_position = observation("bullet")("base")("position");
+  base_position = interface_->transform_base_to_world().block<3, 1>(0, 3);
   ASSERT_NEAR(base_position.x(), 0.0, 1e-4);
   ASSERT_NEAR(base_position.y(), 0.0, 1e-4);
   ASSERT_NEAR(base_position.z(), 0.0, 1e-4);
@@ -354,15 +353,13 @@ TEST_F(BulletInterfaceTest, BasePositionFreeFall) {
   const double T = 0.05;  // seconds
   for (double t = 0.0; t < T; t += dt_) {
     interface_->cycle([](const moteus::Output& output) {});
-    interface_->observe(observation);
-    base_position = observation("bullet")("base")("position");
-    spdlog::info("at t={} we are at z={}", t, base_position.z());
+    base_position = interface_->transform_base_to_world().block<3, 1>(0, 3);
   }
 
-  base_position = observation("bullet")("base")("position");
+  base_position = interface_->transform_base_to_world().block<3, 1>(0, 3);
   ASSERT_NEAR(base_position.x(), 0.0, 1e-4);
   ASSERT_NEAR(base_position.y(), 0.0, 1e-4);
-  ASSERT_NEAR(base_position.z(), -0.5 * 9.81 * T * T, 1e-4);
+  ASSERT_NEAR(base_position.z(), -0.5 * 9.81 * T * T, 1e-3);
 }
 
 }  // namespace vulp::actuation
